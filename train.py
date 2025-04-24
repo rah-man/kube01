9f914e1256298f5202a14894391d7a7b6403b6f9
Fahrurrozi Rahman <11063604+rah-man@users.noreply.github.com>
2025-04-24 13:37:15 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,39 @@
+import argparse
+import os
 import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+
+def get_arg_or_env(arg_name, env_name, default, arg_type=str):
+    env_value = os.getenv(env_name)
+    if env_value is not None:
+        return arg_type(env_value)
+    return default
+
+parser = argparse.ArgumentParser(description="Train a HuggingFace model")
+
+parser.add_argument(
+    "--model",
+    type=str,
+    default=get_arg_or_env("model", "MODEL", "bert-base-uncased"),
+    help="Name of the model"
+)
+
+parser.add_argument(
+    "--epochs",
+    type=int,
+    default=get_arg_or_env("epochs", "EPOCHS", 3, int),
+    help="Number of epochs"
+)
+
+parser.add_argument(
+    "--lr",
+    type=float,
+    default=get_arg_or_env("lr", "LR", 5e-5, float),
+    help="Learning rate"
+)
+
+args = parser.parse_args()
 
 # Load dataset
 dataset = load_dataset("imdb")
@@ -8,9 +41,9 @@ train_data = dataset["train"].shuffle(seed=42).select(range(1000))  # Small subs
 test_data = dataset["test"].shuffle(seed=42).select(range(500))  # Small subset
 
 # Load tokenizer and model
-model_name = "distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+# model_name = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(args.model)
+model = AutoModelForSequenceClassification.from_pretrained(args.model, num_labels=2)
 
 # Tokenize data
 def preprocess(examples):
@@ -28,7 +61,7 @@ training_args = TrainingArguments(
     logging_steps=10,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
-    num_train_epochs=1,
+    num_train_epochs=args.epochs,
     report_to="none",
 )
 
